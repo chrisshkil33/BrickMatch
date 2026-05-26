dd485535d68bc40ec7a885d60a2634ee6b67a394
chrisshkil33 <chrisshkil33@gmail.com>
2026-05-26 13:21:35 -0400

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import BrickMatch from './brickmatch.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrickMatch />
+  </React.StrictMode>
+)
